13a45c09fa683202de5edbdf23421597f9f8dc61
pinus0711 <yyis3173@gmail.com>
2024-04-12 16:24:51 +0900

    add cardInfoController

--- src/main/java/com/team7/controller/CardInfoController.java
+++ src/main/java/com/team7/controller/CardInfoController.java
@@ -1,14 +1,10 @@
 package com.team7.controller;
 
-import com.team7.db.dto.CardDto;
-import com.team7.model.entity.Benefit;
+import com.team7.dto.CardDto;
 import com.team7.model.entity.Card;
 import com.team7.service.relationship.CardBenefitService;
 import com.team7.service.entitiy.CardService;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
-=======
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.Logger;
@@ -16,18 +12,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
->>>>>>> 308e3c8dd21fb6a5b2777811aa31670508629e57
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@CrossOrigin(originPatterns = "http://localhost:3000",allowCredentials = "true")
+@CrossOrigin(originPatterns = "http://localhost:3000")
 @RestController
-@RequestMapping("/cards")
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/cards")
@@ -36,19 +23,6 @@ public class CardInfoController {
     public final CardBenefitService cardBenefitService;
 
     @GetMapping("/")
-<<<<<<< HEAD
-    public List<CardDto> showAllCards(){
-        ArrayList<Card> cardList=cardService.findAll();
-        List<CardDto> cardDtos = cardList.stream()
-                .map(o->new CardDto(o))
-                .collect(Collectors.toList());
-
-        return cardDtos;
-    }
-
-    @GetMapping("/card/{id}")
-    public CardDto CardgetCardByUid(@PathVariable Long id){
-=======
     public ArrayList<CardDto> getCards(){
         return cardService.cardsToCardDtos(cardService.findAllCards());
     }
@@ -75,27 +49,8 @@ public class CardInfoController {
 
     @GetMapping("/card/{id}")
     public CardDto getCardByUid(@PathVariable Long id){
->>>>>>> 308e3c8dd21fb6a5b2777811aa31670508629e57
         Card card = cardService.findCardByCardUid(id);
         CardDto rv = new CardDto(card);
         return rv;
     }
-
-    @GetMapping("/{type}")
-    public List<CardDto> showByType(@PathVariable String type) {
-        String convertType="";
-        if(type.equals("credit")){
-            convertType="신용";
-        }
-        else if(type.equals("check")){
-            convertType="체크";
-        }
-        ArrayList<Card> cardList = cardService.findCardsByType(convertType);
-        List<CardDto> cardDtos = cardList.stream()
-                .map(card -> new CardDto(card))
-                .collect(Collectors.toList());
-
-        return cardDtos;
-    }
-
 }
